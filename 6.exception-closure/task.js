3bc99b6616e0f32a24180dc3a35ea5c123835dd0
OlegBirykov <p_petro75@mail.ru>
2021-06-08 22:10:49 +0300

    Выполнено ДЗ по теме 6

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,49 @@
+'use strict'
+
+function parseCount(value) {
+  const result = Number.parseInt(value);
+  if(isNaN(result)) {
+    throw new Error('Невалидное значение');
+  }
+  return result;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (e) {
+    return e;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if ((a + b < c) || (a + c < b) || (b + c < a)) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  getArea() {
+    const p = this.getPerimeter() / 2;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    const error = 'Ошибка! Треугольник не существует';
+    return {
+      getArea: () => error,
+      getPerimeter: () => error,
+    }
+  }
+}
