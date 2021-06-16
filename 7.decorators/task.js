a03e13aba6b6dc66b192e058bd27eef5cd5dbbda
unknown <p_petro75@mail.ru>
2021-06-16 16:43:52 +0300

    Выполнено задание 1 по теме 7

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,7 +1,28 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
-}
+'use strict';
 
+function cachingDecoratorNew(func) {
+  const cache = [];
+
+  return (...args) => {
+    const hash = args.join(',');
+    const index = cache.findIndex((item) => item.hash === hash);
+    let result;
+
+    if (index !== -1) {
+      result = 'Из кэша: ' + cache[index].value;
+    } else {
+      const value = func(...args);
+      cache.push({ hash, value });
+      if (cache.length > 5) { 
+        cache.shift();
+      }
+      result = 'Вычисляем: ' + value; 
+    }
+
+    console.log(result);
+    return result;
+  }
+}
 
 function debounceDecoratorNew(func) {
   // Ваш код
